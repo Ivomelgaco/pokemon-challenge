7986c6da96f27efb1f9c8e6046ce9fdc52550217
Ivonel <ivonel.am94@gmail.com>
2023-11-08 01:07:09 +0000

    Criando paginas e rotas

---
+++ pokemon-challenge/src/Pages/Home/index.jsx
@@ -0,0 +1,27 @@
+import NavBar from "../../Components/NavBar";
+import MainButton from "../../Components/mainButton";
+import "./style.css";
+
+function Home() {
+  return (
+    <div className="bg">
+      <NavBar />
+      <img
+        className="banner"
+        src="/BannerComplete.png"
+        alt="Imagem do Pikaxu"
+      />
+      <h1 className="title">
+        <span className="bold-text">Find</span> all your
+        <br /> favorite <span className="bold-text">pokemon</span>
+      </h1>
+      <p className="paragraph">
+        You can know the type of Pokemon, its strengths,
+        <br /> disadvantages and abilities.
+      </p>
+      <MainButton />
+    </div>
+  );
+}
+
+export default Home;
---
+++ pokemon-challenge/src/Pages/Pokedex/index.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../../Components/NavBar";
+import "./style.css";
+
+function Pokedex() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default Pokedex;
