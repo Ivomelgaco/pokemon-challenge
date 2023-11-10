2c892c9fa8a3b2c051a5f882ea80327ea4c263d0
Ivonel <ivonel.am94@gmail.com>
2023-11-10 00:45:33 +0000

    Finalizando estruturas das paginas

--- pokemon-challenge/src/Components/Card/index.jsx
+++ pokemon-challenge/src/Components/Card/index.jsx
@@ -1,14 +1,18 @@
 import "./style.css";
 
-function Card() {
+// eslint-disable-next-line react/prop-types
+function Card({ name, imageUrl, type }) {
   return (
     <div className="card-container">
       <div className="box-left">
-        <div></div>
-        <div></div>
-        <div></div>
+        {/* Adicione uma imagem do Pokémon */}
+        <img src={imageUrl} alt={name} />
       </div>
-      <div className="box-right"></div>
+      <div className="box-right">
+        {/* Adicione o nome e o tipo do Pokémon */}
+        <h3>{name}</h3>
+        <p>Type: {type}</p>
+      </div>
     </div>
   );
 }
